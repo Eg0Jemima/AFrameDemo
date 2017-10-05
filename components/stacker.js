74a679619ce8a305996a0c7356ea414725b04e16
Eg0Jemima <Eg0Jemima@gmail.com>
2017-10-05 16:11:07 -0400

    Ordering of notifications perfected - Still need to add swap animations - Still need to handle removal of notifications appropriately

--- components/stacker.js
+++ components/stacker.js
@@ -1,22 +1,34 @@
 var scene = document.querySelector("a-scene");
 var list = [];
 var count = 0;
+var max = 3;
 
 document.addEventListener("DOMContentLoaded", function(event) {
    scene = document.querySelector("a-scene");
 
    // Listen for the event.
    document.addEventListener('notificationAdded', function () {
+       //remove first element from list so it cannot surpass max
+       if(list.length > max){
+           list.shift();
+           //console.log(list);
+       }
+
        for (let i = 0; i < list.length; i++) {
            console.log(list[i]);
            switch(i){
                case 0:
-                   list[i].setAttribute("position", {x: 0, y: .05, z: -.06});
+                   if(list.length == 1){
+                       list[i].setAttribute("position", {x: 0, y: .0215, z: -.05});
+                   } else {
+                       list[i].setAttribute("position", {x: 0, y: .04, z: -.06});
+                   }
                    break;
                case 1:
+                   list[i].setAttribute("position", {x: 0, y: .0215, z: -.05});
                    break;
                case 2:
-                   list[i].setAttribute("position", {x: 0, y: .03, z: -.06});
+                   list[i].setAttribute("position", {x: 0, y: .01, z: -.06});
                    break;
                default:
                    break;
@@ -29,20 +41,21 @@ document.addEventListener("DOMContentLoaded", function(event) {
 document.addEventListener("keypress", function(e){
     console.log(e);
     if(e.charCode == 110){
-        list.push(addNotification(1));
+        list.push(addNotification());
         console.log(list);
-        
+
         // Dispatch the event.
         document.dispatchEvent(new Event('notificationAdded'));
     }
 });
 
-function addNotification(count){
+function addNotification(){
+    count++;
     let notification = document.createElement("a-entity");
     let val = "This is notification " + count;
     setAttributes(notification, {
         "notification": "text:" + val,
-        "position": {x: 0, y: .035, z: -.05},
+        "position": {x: 0, y: .025, z: -.05},
         "scale": {x: .004, y: .004, z: .004}
     });
 
